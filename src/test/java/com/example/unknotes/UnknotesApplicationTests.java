b537b4ebd8626666eb890b001b11fd00a7f6cf82
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2022-04-07 07:20:14 +0100

    chore: initial commit
    Bootstrap app following https://github.com/dsyer/spring-boot-angular

---
+++ src/test/java/com/example/unknotes/UnknotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unknotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnknotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
