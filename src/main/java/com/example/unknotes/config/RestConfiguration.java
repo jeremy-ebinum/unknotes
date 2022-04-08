6729ba2473982e2dbecbca6ff36e3cbd624df052
Jeremy Ebinum <jeremy.ebinum@gmail.com>
2022-04-08 10:21:36 +0100

    fix: update Spring data rest config

--- src/main/java/com/example/unknotes/config/RestConfiguration.java
+++ src/main/java/com/example/unknotes/config/RestConfiguration.java
@@ -2,14 +2,25 @@ package com.example.unknotes.config;
 
 import com.example.unknotes.model.Note;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @Configuration
-public class RestConfiguration implements RepositoryRestConfigurer {
-  public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
-    config.exposeIdsFor(Note.class);
-    config.setBasePath("/api");
+public class RestConfiguration {
+
+  @Bean
+  public RepositoryRestConfigurer repositoryRestConfigurer() {
+    return new RepositoryRestConfigurer() {
+
+      @Override
+      public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        config.exposeIdsFor(Note.class);
+        config.setBasePath("/api");
+      }
+
+    };
   }
 }
